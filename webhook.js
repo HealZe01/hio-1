255d7540a7ea94925132ab32c3a3bea4a11ea98f
Hlz110 <xvminkk7@gmail.com>
2025-01-05 14:11:36 +0100

    modif

--- webhook.js
+++ webhook.js
@@ -2,18 +2,29 @@ const { Webhook, MessageBuilder } = require("discord-webhook-node");
 const fetch = require("isomorphic-fetch");
 
 const botToken = process.env.bot_token;
-const channelID = "1325158174189420585";
+const channelID = "1325158174189420585";  // Canal ID
+
 async function sendWebhook(channelID, embed) {
-  const createUrl = `https://discord.com/api/v10/channels/${1325158174189420585}/webhooks`;
+  // Utilisation de channelID dans l'URL
+  const createUrl = `https://discord.com/api/v10/channels/${channelID}/webhooks`;
+  
   try {
+    // Création du webhook
     const createResponse = await fetch(createUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json", Authorization: `Bot ${botToken}` },
       body: JSON.stringify({ name: "Logger Webhook" }),
     });
+
+    // Vérifier la réponse
     const createData = await createResponse.json();
+    console.log(createData); // Ajouter pour déboguer et vérifier la réponse de Discord
+
+    // Utilisation de l'ID et du token du webhook
     const webhookUrl = `https://discord.com/api/v10/webhooks/${createData.id}/${createData.token}`;
     await new Webhook(webhookUrl).send(embed);
+
+    // Suppression du webhook après utilisation
     await fetch(`https://discord.com/api/v10/webhooks/${createData.id}/${createData.token}`, {
       method: "DELETE",
       headers: { Authorization: `Bot ${botToken}` },
